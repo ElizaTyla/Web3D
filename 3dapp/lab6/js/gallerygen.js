01b74f17aa58dc25d3fe6003a4bc710b9c8e353c
Eliza Back <elizatyla@gmail.com>
2023-05-18 12:42:17 +0100

    Lab6 - Eliza

--- 3dapp/lab6/js/gallerygen.js
+++ 3dapp/lab6/js/gallerygen.js
@@ -30,8 +30,7 @@ $(document).ready(function() {
 			for (var i=0;i<response.length;i++) {
 				htmlCode += '<a data-fslightbox href="'+ response[i] + '" > ';
 				htmlCode += '<img class="card-img-top img-thumbnail" src="' + response[i] + '"/>';
-				htmlCode += '</a>';	
-				console.log(response[i]);		
+				htmlCode += '</a>';		
 			}
 			var galleries = document.getElementsByClassName('gallerySpace');
 			for (var j=0;j<galleries.length;j++) {galleries[j].innerHTML = htmlCode}
